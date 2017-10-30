15879f57555d5c33cbd4f261301f5292c01cc09e
krustybr <carlos.poles@griffithuni.edu.au>
2017-10-30 10:16:10 +1000

    Initial Commit

---
+++ Stockz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by Carlos Poles on 30/10/17.
+//  Copyright © 2017 Carlos Poles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
